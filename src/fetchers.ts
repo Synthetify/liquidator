db49f8db1383f5a3900558b0d55191673a6c7069
Mateusz Zając <matzayonc@gmail.com>
2022-01-06 19:11:12 +0100

    added methods to fetch

---
+++ src/fetchers.ts
@@ -0,0 +1,47 @@
+import { Idl } from '@project-serum/anchor'
+import { Connection, Account, PublicKey, AccountInfo } from '@solana/web3.js'
+import {
+  ExchangeAccount,
+  AssetsList,
+  ExchangeState,
+  Exchange,
+  VaultEntry,
+  Vault
+} from '@synthetify/sdk/lib/exchange'
+import EXCHANGE_IDL from '../exchange.json'
+import { AccountsCoder, BN } from '@project-serum/anchor'
+
+const coder = new AccountsCoder(EXCHANGE_IDL as Idl)
+
+export const fetchExchangeAccounts = async (connection: Connection, exchangeProgram: PublicKey) => {
+  const accounts = await connection.getProgramAccounts(exchangeProgram, {
+    filters: [{ dataSize: 1412 + 8 }]
+  })
+
+  return accounts.map(({ account }) => parseUser(account))
+}
+
+export const fetchVaultEntries = async (connection: Connection, exchangeProgram: PublicKey) => {
+  const accounts = await connection.getProgramAccounts(exchangeProgram, {
+    filters: [{ dataSize: 116 + 8 }]
+  })
+
+  return accounts.map(({ account }) => parseVaultEntry(account))
+}
+
+export const fetchVaults = async (connection: Connection, exchangeProgram: PublicKey) => {
+  const accounts = await connection.getProgramAccounts(exchangeProgram, {
+    filters: [{ dataSize: 376 + 8 }]
+  })
+
+  return accounts.map(({ account }) => parseVault(account))
+}
+
+export const parseUser = (account: AccountInfo<Buffer>) =>
+  coder.decode<ExchangeAccount>('ExchangeAccount', account.data)
+
+export const parseVaultEntry = (account: AccountInfo<Buffer>) =>
+  coder.decode<VaultEntry>('VaultEntry', account.data)
+
+export const parseVault = (account: AccountInfo<Buffer>) =>
+  coder.decode<Vault>('Vault', account.data)
