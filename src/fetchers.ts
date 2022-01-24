3bf2d9d2f9b771ee9a43be93661b7f0e8763a5f6
Mateusz Zając <matzayonc@gmail.com>
2022-01-24 19:42:53 +0100

    moved to new idl

--- src/fetchers.ts
+++ src/fetchers.ts
@@ -1,55 +1,49 @@
 import { Idl } from '@project-serum/anchor'
 import { Connection, PublicKey, AccountInfo } from '@solana/web3.js'
-import {
-  ExchangeAccount,
-  AssetsList,
-  ExchangeState,
-  Exchange,
-  VaultEntry,
-  Vault
-} from '@synthetify/sdk/lib/exchange'
-import EXCHANGE_IDL from '../exchange.json'
+import { ExchangeAccount, VaultEntry, Vault } from '@synthetify/sdk/lib/exchange'
+import { IDL } from '@synthetify/sdk/lib/idl/exchange'
 import { AccountsCoder, BN } from '@project-serum/anchor'
-
-const coder = new AccountsCoder(EXCHANGE_IDL as Idl)
 
 export const fetchExchangeAccounts = async (connection: Connection, exchangeProgram: PublicKey) => {
   const accounts = await connection.getProgramAccounts(exchangeProgram, {
     filters: [{ dataSize: 1412 + 8 }]
   })
+  const coder = new AccountsCoder(IDL as Idl)
 
-  return accounts.map(({ account }) => parseUser(account))
+  return accounts.map(({ account }) => parseUser(account, coder))
 }
 
 export const fetchVaultEntries = async (connection: Connection, exchangeProgram: PublicKey) => {
   const accounts = await connection.getProgramAccounts(exchangeProgram, {
     filters: [{ dataSize: 116 + 8 }]
   })
+  const coder = new AccountsCoder(IDL as Idl)
 
-  return accounts.map(({ account }) => parseVaultEntry(account))
+  return accounts.map(({ account }) => parseVaultEntry(account, coder))
 }
 
 export const fetchVaults = async (connection: Connection, exchangeProgram: PublicKey) => {
   const accounts = await connection.getProgramAccounts(exchangeProgram, {
     filters: [{ dataSize: 376 + 8 }]
   })
+  const coder = new AccountsCoder(IDL as Idl)
 
   return accounts.map(({ account, pubkey }): Account<Vault> => {
     return {
-      data: parseVault(account),
+      data: parseVault(account, coder),
       address: pubkey
     }
   })
 }
 
-export const parseUser = (account: AccountInfo<Buffer>) =>
-  coder.decode<ExchangeAccount>('ExchangeAccount', account.data)
+export const parseUser = (account: AccountInfo<Buffer>, coder: AccountsCoder) =>
+  coder.decode<ExchangeAccount>('exchangeAccount', account.data)
 
-export const parseVaultEntry = (account: AccountInfo<Buffer>) =>
-  coder.decode<VaultEntry>('VaultEntry', account.data)
+export const parseVaultEntry = (account: AccountInfo<Buffer>, coder: AccountsCoder) =>
+  coder.decode<VaultEntry>('vaultEntry', account.data)
 
-export const parseVault = (account: AccountInfo<Buffer>) =>
-  coder.decode<Vault>('Vault', account.data)
+export const parseVault = (account: AccountInfo<Buffer>, coder: AccountsCoder) =>
+  coder.decode<Vault>('vault', account.data)
 
 export interface Account<T> {
   data: T
